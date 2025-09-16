05c1c3ff575da39ef51e6b0986e3e7621832aa3e
Manojgit45 <manojnani90521@gmail.com>
2025-09-16 14:45:24 +0530

    Update stop_container.sh
    updated again

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-cotainer_id=`docker ps | awk -f " " '{print $1}'`
+cotainer_id=`docker ps| cut -f1 -d " "|tail -1`
 # Stop the running container (if any)
 docker rm -f $container_id
